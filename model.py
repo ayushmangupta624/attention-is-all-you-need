60898629320493dabef0c783cb52f0bb7620cb71
Ayushman Gupta <ayushman2346@gmail.com>
2025-05-27 22:54:11 +0530

    encoder

--- model.py
+++ model.py
@@ -36,6 +36,106 @@ class PositionalEncoding(nn.Module):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
         return self.dropout(x)
 
+
+class Norm(nn.Module): 
+    def __init__(self, eps: float = 10**-6): 
+        super().__init__()
+        self.epsilon = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #Multiplicative
+        self.beta = nn.Parameter(torch.zeros(1)) #Additive
+
+        
+    def forward(self, x): 
+        x = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x-mean) / (std + self.eps) + self.beta
+
+class FeedForward(nn.Module): 
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None: 
+        super().__init__(): 
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x): 
+        return self.fc2(self.dropout(self.relu(self.fc1(x))))
+
+class MultiHead(nn.Module): 
+
+    def __init__(self, h: int, d_model: int, dropout: float) -> None:
+       super().__init__()
+       assert d_model % h == 0, "d_model is not divisible by h"
+       self.d_k = d_model // h
+       self.W_Q = nn.Linear(d_model, d_model)
+       self.W_K = nn.Linear(d_model, d_model)
+       self.W_V = nn.Linear(d_model, d_model) 
+       self.W_O = nn.Linear(d_model, d_model)
+       self.dropout(nn.Dropout)
+
+    @staticmethod
+    def self_attention(query, key, value, mask: int, dropout: nn.Dropout)
+        #(batch, h, seq_len, d_k) -> (batch, h, seq_len, seq_len)
+       
+        d_k = key.shape[-1]
+        attention_logits = (query @ key.transpose(2, 3)) / math.sqrt(d_k)
+        if mask is not None: 
+            attention_logits = attention_logits.masked_fill_(mask == 0, -1e9)
+        attention_logits = attention_logits.softmax(dim=-1)
+        if dropout is not None: 
+            attention_logits = dropout(attention_logits)
+
+        return (attention_logits @ value), attention_logits #return for visualization of logits
+
+
+    def forward(self, q, k, v, mask): 
+        Q = self.W_Q(q)
+        K = self.W_Q(k)
+        V = self.W_Q(v)
+
+        Q = Q.view(Q.shape[0], Q.shape[1], h, self.d_k).transpose(1, 2)
+        K = K.view(K.shape[0], K.shape[1], h, self.d_k).transpose(1, 2)
+        V = V.view(V.shape[0], V.shape[1], h, self.d_k).transpose(1, 2)
+
+        x, self.attention_logits = MultiHead.self_attention(Q, K, V, mask, self.dropout)
+      
+        #(batch, h, seq_len, seq_len) -> (batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, h * self.d_k)
+         return self.W_O(x)
+
+class ResidualConnection(nn.Module): 
+    def __init__(self, dropout: float) -> None: 
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = Norm()
+
+    def forward(self, x, prev_layer): 
+        return x + self.dropout(self.norm(prev_layer(x)))
+
+class EncoderBlock(nn.Module): 
+    def __init__(self, multi_head: MultiHead, ff: FeedForward, dropout: float) -> None: 
+        super().__init__()
+        self.multi_head_attention = multi_head
+        self.feed_forward = self.ff
+        self.dropout = nn.Dropout(dropout)
+        self.residual = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, mask):
+        x = self.residual[0](x, lambda x: self.multi_head_attention(x, x, x, mask))
+        x = self.residual[1](x, self.feed_forward)
+        return x 
+
+class Encoder(nn.Module): 
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = Norm()
+
+    def forward(self, x, mask): 
+        for layer in layers: 
+            x = layer(x, mask)
+        return self.norm(x)   
+
         
 
 
